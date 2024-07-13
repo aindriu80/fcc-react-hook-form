1f4f700b1dcbec3dcc017ae9d880d20bef749426
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2024-07-13 18:49:52 +0100

    Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Users from './users/components/Users';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Hook Form with Zod</h1>
+      <Users />
+    </div>
+  );
+}
+
+export default App;
