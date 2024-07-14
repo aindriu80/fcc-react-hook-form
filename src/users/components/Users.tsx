93a2b999939653526f2590f793a752a551b52af8
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2024-07-14 19:20:05 +0100

    A simple form using react-hook-form

--- src/users/components/Users.tsx
+++ src/users/components/Users.tsx
@@ -1,5 +1,27 @@
+import { useForm } from 'react-hook-form';
 const Users = () => {
-  return <div>Users </div>;
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<{ email: string }>({ mode: 'all' });
+
+  const onSubmit = () => {
+    console.log('submit');
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        {...register('email', {
+          required: { value: true, message: 'The email is required' },
+          maxLength: { value: 10, message: 'Too many characters' },
+        })}
+        placeholder="Email"
+      />
+      <p>{errors.email?.message}</p>
+    </form>
+  );
 };
 
 export default Users;
