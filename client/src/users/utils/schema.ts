cedd3e94a60e48e686e810ed88c1d94a5a342a79
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2024-07-29 15:59:58 +0100

    Use of Typescript intersection and discriminated union in Zod

--- client/src/users/utils/schema.ts
+++ client/src/users/utils/schema.ts
@@ -1,27 +1,39 @@
 import { z } from 'zod';
 import { patterns } from '../../constants';
 
-export const schema = z.object({
-  name: z.string().min(1, { message: 'Required' }),
-  email: z
-    .string()
-    .min(1, { message: 'Email is required' })
-    .refine((text) => patterns.email.test(text), {
-      message: 'Email not valid',
-    }),
-  states: z.array(z.string().min(1).max(2)),
-  languagesSpoken: z.array(z.string()),
-  gender: z.string().min(1),
-  skills: z.array(z.string()).max(2),
-  registrationDateAndTime: z.date(),
-  formerEmploymentPeriod: z.array(z.date()).min(2).max(2),
-  salaryRange: z.array(z.number()).min(2).max(2),
-  isTeacher: z.boolean(),
-});
+type Type1 = {};
+type Type2 = {};
+type Type3 = {};
+
+type type4 = Type1 & (Type2 | Type3);
+
+export const schema = z.intersection(
+  z.object({
+    name: z.string().min(1, { message: 'Required' }),
+    email: z
+      .string()
+      .min(1, { message: 'Email is required' })
+      .refine((text) => patterns.email.test(text), {
+        message: 'Email not valid',
+      }),
+    states: z.array(z.string().min(1).max(2)),
+    languagesSpoken: z.array(z.string()),
+    gender: z.string().min(1),
+    skills: z.array(z.string()).max(2),
+    registrationDateAndTime: z.date(),
+    formerEmploymentPeriod: z.array(z.date()).min(2).max(2),
+    salaryRange: z.array(z.number()).min(2).max(2),
+    isTeacher: z.boolean(),
+  }),
+
+  z.discriminatedUnion('variant', [z.object({ variant: z.literal('create') })]),
+  z.object({ variant: z.literal('edit'), id: z.string().min(1) }),
+);
 
 export type Schema = z.infer<typeof schema>;
 
 export const defaultValues: Schema = {
+  variant: 'create',
   email: '',
   name: '',
   states: [],
